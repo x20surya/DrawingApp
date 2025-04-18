364763b01722c4addd40da4630348b90f989e4a1
x20surya <suryanshuc659@gmail.com>
2025-04-18 12:58:57 +0530

    feat: Add color select and save image

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <raylib.h>
 #include <string>
 
+#define MAX_COLORS_COUNT 23
+
 int main(void)
 {
 
@@ -8,6 +10,33 @@ int main(void)
 	const int screenHeight = 450;
 
 	InitWindow(screenWidth, screenHeight, "Drawing app");
+
+	Color colors[MAX_COLORS_COUNT] = {
+		RAYWHITE, YELLOW, GOLD, ORANGE, PINK, RED, MAROON, GREEN, LIME, DARKGREEN,
+		SKYBLUE, BLUE, DARKBLUE, PURPLE, VIOLET, DARKPURPLE, BEIGE, BROWN, DARKBROWN,
+		LIGHTGRAY, GRAY, DARKGRAY, BLACK
+	};
+
+	Rectangle colorRectangles[MAX_COLORS_COUNT] = { 0 };
+
+	for (int i = 0; i < MAX_COLORS_COUNT; i++)
+	{
+		colorRectangles[i].x = 10 + 30.0f * i + 2 * i;
+		colorRectangles[i].y = 10;
+		colorRectangles[i].width = 30;
+		colorRectangles[i].height = 30;
+	}
+
+	int colorSelected = 0;
+	int colorSelectedPrev = colorSelected;
+	int colorMouseHover = 0;
+	float brushSize = 10.0f;
+	bool mouseWasPressed = false;
+
+	Rectangle btnSaveRec = { 750, 10, 40, 30 };
+	bool btnSaveMouseHover = false;
+	bool showSaveMessage = false;
+	int saveMessageCounter = 0;
 
 	RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
 
@@ -24,6 +53,32 @@ int main(void)
 		
 		Vector2 mousePos = GetMousePosition();
 
+		if (IsKeyPressed(KEY_RIGHT)) colorSelected++;
+		else if (IsKeyPressed(KEY_LEFT)) colorSelected--;
+
+		if (colorSelected >= MAX_COLORS_COUNT) colorSelected = 0;
+		else if (colorSelected < 0) colorSelected = MAX_COLORS_COUNT - 1;
+
+		for (int i = 0; i < MAX_COLORS_COUNT; i++)
+		{
+			if (CheckCollisionPointRec(mousePos, colorRectangles[i]))
+			{
+				colorMouseHover = i;
+				break;
+			}
+			else colorMouseHover = -1;
+		}
+
+		if ((colorMouseHover >= 0) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+		{
+			colorSelected = colorMouseHover;
+			colorSelectedPrev = colorSelected;
+		}
+
+		brushSize += GetMouseWheelMove() * 5;
+		if (brushSize < 2) brushSize = 2;
+		if (brushSize > 50) brushSize = 50;
+
 		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
 		{
 			BeginTextureMode(target);
@@ -31,8 +86,11 @@ int main(void)
 			{
 				prevMousePos = mousePos;
 			}
-			DrawLineEx({ prevMousePos.x,prevMousePos.y }, { mousePos.x, mousePos.y }, 10.0f, BLUE);
-			DrawCircle((int)mousePos.x, (int)mousePos.y, 5.0f, BLUE);
+			if (mousePos.y > 50)
+			{
+				DrawLineEx({ prevMousePos.x,prevMousePos.y }, { mousePos.x, mousePos.y }, brushSize*2, colors[colorSelected]);
+				DrawCircle((int)mousePos.x, (int)mousePos.y, brushSize, colors[colorSelected]);
+			}
 			EndTextureMode();
 
 			prevMousePos = mousePos;
@@ -41,11 +99,51 @@ int main(void)
 		{
 			prevMousePos = { 0,0 };
 		}
-		
+
+		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+		{
+			if (!mouseWasPressed)
+			{
+				colorSelectedPrev = colorSelected;
+				colorSelected = 0;
+			}
+
+			mouseWasPressed = true;
+
+			BeginTextureMode(target);
+			if (mousePos.y > 50) DrawCircle((int)mousePos.x, (int)mousePos.y, brushSize, colors[0]);
+			EndTextureMode();
+		}
+		else if (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT) && mouseWasPressed)
+		{
+			colorSelected = colorSelectedPrev;
+			mouseWasPressed = false;
+		}
+
+		if (CheckCollisionPointRec(mousePos, btnSaveRec)) btnSaveMouseHover = true;
+		else btnSaveMouseHover = false;
+
+		if (btnSaveMouseHover && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) || IsKeyPressed(KEY_S))
+		{
+			Image image = LoadImageFromTexture(target.texture);
+			ImageFlipVertical(&image);
+			ExportImage(image, "painting.png");
+			UnloadImage(image);
+			showSaveMessage = true;
+		}
+		if (showSaveMessage)
+		{
+			saveMessageCounter++;
+			if (saveMessageCounter > 240)
+			{
+				showSaveMessage = false;
+				saveMessageCounter = 0;
+			}
+		}
 		if (IsKeyPressed(KEY_C))
 		{
 			BeginTextureMode(target);
-			ClearBackground(WHITE);
+			ClearBackground(RAYWHITE);
 			EndTextureMode();
 		}
 
@@ -55,14 +153,42 @@ int main(void)
 
 		DrawTextureRec(target.texture, { 0, 0, (float)target.texture.width, -(float)target.texture.height }, { 0, 0 }, WHITE);
 
-		DrawCircle(GetMouseX(), GetMouseY(), 5.0f, BLUE);
+		if (mousePos.y > 50)
+		{
+			if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) DrawCircleLines((int)mousePos.x, (int)mousePos.y, brushSize, GRAY);
+			else DrawCircle(GetMouseX(), GetMouseY(), brushSize, colors[colorSelected]);
+		}
+
+		DrawRectangle(0, 0, GetScreenWidth(), 50, RAYWHITE);
+		DrawLine(0, 50, GetScreenWidth(), 50, LIGHTGRAY);
+
+		for (int i = 0; i < MAX_COLORS_COUNT; i++) DrawRectangleRec(colorRectangles[i], colors[i]);
+		DrawRectangleLines(10, 10, 30, 30, LIGHTGRAY);
+
+		if (colorMouseHover >= 0) DrawRectangleRec(colorRectangles[colorMouseHover], Fade(WHITE, 0.6f));
+
+		DrawRectangleLinesEx({
+			colorRectangles[colorSelected].x - 2, colorRectangles[colorSelected].y - 2,
+				colorRectangles[colorSelected].width + 4, colorRectangles[colorSelected].height + 4
+		}, 2, BLACK);
+
+		DrawRectangleLinesEx(btnSaveRec, 2, btnSaveMouseHover ? RED : BLACK);
+		DrawText("SAVE!", 755, 20, 10, btnSaveMouseHover ? RED : BLACK);
+
+		if (showSaveMessage)
+		{
+			DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(RAYWHITE, 0.8f));
+			DrawRectangle(0, 150, GetScreenWidth(), 80, BLACK);
+			DrawText("IMAGE SAVED:  my_amazing_texture_painting.png", 150, 180, 20, RAYWHITE);
+		}
 
 		DrawFPS(10, 10);
 
-		std::string text = "Press 'C' to clear the screen";
+		std::string clearScreenText = "Press 'C' to clear the screen";
+		std::string brushSizeControlText = "Use Mouse Wheel to change brush size";
 
-		DrawText(text.c_str(), screenWidth - (text.length() * 12), screenHeight - 20, 20, DARKGRAY);
-
+		DrawText(clearScreenText.c_str(), screenWidth - (clearScreenText.length() * 12), screenHeight - 25, 20, DARKGRAY);
+		DrawText(brushSizeControlText.c_str(), screenWidth - (brushSizeControlText.length() * 12 + clearScreenText.length() * 12), screenHeight - 25, 20, DARKGRAY);
 		EndDrawing();
 	}
 
